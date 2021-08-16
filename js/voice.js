e9488927889c1bd5352d8372fde9b51a67a6c770
Danushravichandran <52316360+Danushravichandran@users.noreply.github.com>
2021-08-16 09:37:39 +0530

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -1,54 +1,39 @@
+const btn=document.querySelector('.talk');
+const content=document.querySelector('.content');
+
+
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-recognition.interimResults = true;
 
-let p = document.createElement("p");
+recognition.onstart()=function()
+{
+  alert('You can speak in this browser');
+}
 
+recognition.onresult()=function(event)
+{
+  const current=event.resultIndex;
+  
+  const transcript=event.results[current][0].transcript;
+  content.textContent=transcript;
+  gohere(transcript);
+  
+   console.log(event);
+}
 
-recognition.addEventListener("result", (e) => {
-  texts.appendChild(p);
-  const text = Array.from(e.results)
-    .map((result) => result[0])
-    .map((result) => result.transcript)
-    .join("");
-   console.log(text);
-
-
-  p.innerText = text;
-  if (e.results[0].isFinal) {
-    if (text.includes("how are you")) {
-      p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "I am fine";
-      texts.appendChild(p);
-    }
-    if (
-      text.includes("what's your name") ||
-      text.includes("what is your name")
-    ) {
-      p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "My Name is Danush";
-      texts.appendChild(p);
-    }
-    if (text.includes("open product page")) {
-      p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "opening product page";
-      texts.appendChild(p);
-      console.log("opening pdt page");
-      window.open("https://danushravichandran.github.io/dummywebsite/product.html");
-    }
-    p = document.createElement("p");
-  }
-});
-
-recognition.addEventListener("end", () => {
+btn.addEventListener('click',() =>{
   recognition.start();
 });
 
-recognition.start();
+function gohere(message)
+{
+  if(message=="Go to product page")
+  {
+    console.log("opening pdt page");
+    window.open("https://danushravichandran.github.io/dummywebsite/product.html");
+  }
+}
 
 
 
